6dc278c35bc56f578b937b643d79286c86de513f
Simon de Vlieger <supakeen@redhat.com>
2025-03-24 09:53:46 +0100

    test: some inkling of unit tests
    Initial setup of at least *some* unit tests.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

--- test/conftest.py
+++ test/conftest.py
@@ -1,11 +1,41 @@
 import pytest
 
 
+class MockBaseBuildTask:
+    pass
+
+
+class MockBuildImageTask:
+    pass
+
+
+class MockBuildRoot:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def init(self):
+        pass
+
+    def tmpdir(self, within=False):
+        return ""
+
+    def rootdir(self):
+        return ""
+
+    def expire(self):
+        pass
+
+    def mock(self, args):
+        return 0
+
+
 @pytest.fixture
 def koji_mock_kojid(mocker):
     """Provide things that are *usually* imported from `__main__` in koji
     builder plugins. Which means that `__main__` resolves to `kojid`."""
 
-    mocker.patch("__main__.BaseBuildTask", 1, create=True)
-    mocker.patch("__main__.BuildImageTask", 1, create=True)
-    mocker.patch("__main__.BuildRoot", 1, create=True)
+    mocker.patch("__main__.BaseBuildTask", MockBaseBuildTask, create=True)
+    mocker.patch("__main__.BuildImageTask", MockBuildImageTask, create=True)
+    mocker.patch("__main__.BuildRoot", MockBuildRoot, create=True)
+
+    return mocker
--- test/unit/test_builder.py
+++ test/unit/test_builder.py
@@ -1,8 +1,62 @@
-def test_dummy(koji_mock_kojid):
-    return
+import pytest
 
+
+class MockOptions:
+    def __init__(self, *, topurl=None):
+        self.topurl = topurl
+
+
+def test_arches_for_config(koji_mock_kojid):
     import plugin.builder.image_builder as builder
 
+    assert builder.arches_for_config({"arches": "x86_64"}) == set(["x86_64"])
+    assert builder.arches_for_config({"arches": "x86_64 aarch64"}) == set(
+        ["x86_64", "aarch64"]
+    )
 
-def test_arches_for_config():
-    assert 1 == 1
+
+def test_build_arch_task(koji_mock_kojid):
+    import plugin.builder.image_builder as builder
+
+    t = builder.ImageBuilderBuildArchTask()
+
+    t.id = None
+    t.session = None
+    t.options = MockOptions(topurl="/")
+    t.workdir = None
+
+    t.handler(
+        "Fedora-Minimal",
+        "42",
+        "1",
+        "x86_64",
+        ["minimal-raw"],
+        {"build_tag": "f42-build", "build_tag_name": "f42-build"},
+        {"extra": {"mock.new_chroot": 0}},
+        {"id": 1},
+        {},
+    )
+
+
+def test_build_arch_task_with_data_url_is_exception(koji_mock_kojid):
+    import plugin.builder.image_builder as builder
+
+    t = builder.ImageBuilderBuildArchTask()
+
+    t.id = None
+    t.session = None
+    t.options = MockOptions(topurl="/")
+    t.workdir = None
+
+    with pytest.raises(NotImplementedError):
+        t.handler(
+            "Fedora-Minimal",
+            "42",
+            "1",
+            "x86_64",
+            ["minimal-raw"],
+            {"build_tag": "f42-build", "build_tag_name": "f42-build"},
+            {"extra": {"mock.new_chroot": 0}},
+            {"id": 1},
+            {"data_url": "data"},
+        )
