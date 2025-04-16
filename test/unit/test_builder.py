472aaa58dbab50a28c3e50df7c2ee95316e79cc2
Simon de Vlieger <supakeen@redhat.com>
2025-04-16 15:41:38 +0200

    builder: naively replace `$arch` in repo
    Let's naively replace the `$arch` variable in repository URLs. The
    dependency solver for `osbuild` doesn't quite fully support variable
    expansion at this moment however it will likely do so in the future.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

--- test/unit/test_builder.py
+++ test/unit/test_builder.py
@@ -61,6 +61,52 @@ def test_build_arch_task(koji_mock_kojid):
         ],
     ]
 
+def test_build_arch_task_with_repos(koji_mock_kojid):
+    import plugin.builder.image_builder as builder
+
+    t = builder.ImageBuilderBuildArchTask()
+
+    t.id = None
+    t.session = None
+    t.options = MockOptions(topurl="/")
+    t.workdir = None
+
+    t.handler(
+        "Fedora-Minimal",
+        "42",
+        "1",
+        "x86_64",
+        ["minimal-raw"],
+        {"build_tag": "f42-build", "build_tag_name": "f42-build"},
+        {"extra": {"mock.new_chroot": 0}},
+        {"id": 1},
+        {"repos": ["a/$arch/b"]},
+    )
+
+    assert koji_mock_kojid.buildroot.mock_calls == [
+        [
+            "--cwd",
+            str(koji_mock_kojid.buildroot._tmpdir),
+            "--chroot",
+            "--",
+            "sh",
+            str(koji_mock_kojid.buildroot._tmpdir) + "/mock-wrap",
+            "image-builder",
+            "-v",
+            "build",
+            "--use-librepo=false",
+            "--force-repo",
+            "a/x86_64/b",
+            "--with-sbom",
+            "--with-manifest",
+            "--output-dir",
+            "/builddir/output",
+            "--output-name",
+            "Fedora-Minimal-42-1",
+            "minimal-raw",
+        ],
+    ]
+
 def test_build_arch_task_multiple_types(koji_mock_kojid):
     import plugin.builder.image_builder as builder
 
@@ -127,6 +173,7 @@ def test_build_arch_task_multiple_types(koji_mock_kojid):
             "minimal-raw-zst",
         ],
     ]
+
 
 def test_build_arch_task_ostree(koji_mock_kojid):
     import plugin.builder.image_builder as builder
